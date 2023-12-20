e31869007e65fbbceb08b8da3f30b53699309b33
Zierer, Jonas <jonas.zierer@novartis.com>
2023-12-20 22:20:01 +0100

    fix export

--- cytocipher/tl.py
+++ cytocipher/tl.py
@@ -1,9 +1,8 @@
 #### For cluster scoring ####
-from .score_and_merge.cluster_score import get_markers, \
-                                  code_enrich, coexpr_enrich, giotto_page_enrich
+from .score_and_merge.cluster_score import get_markers, coexpr_enrich, giotto_page_enrich
 
 #### For cluster merging ####
-from .score_and_merge.cluster_merge import merge_clusters, merge
+from .score_and_merge.cluster_merge import merge_clusters, merge, run_enrich
 
 #### For cluster classification ####
 from .score_and_merge.cluster_classifier import merge_clusters_by_classifier, pairwise_classifiers
