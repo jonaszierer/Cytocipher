168b9612f230fa043e43d012b5cbbc1222ee2800
Zierer, Jonas <jonas.zierer@novartis.com>
2023-12-21 08:48:05 +0100

    fix export

--- cytocipher/pl.py
+++ cytocipher/pl.py
@@ -1,5 +1,6 @@
 from .plotting.cluster_diagnostics import enrich_heatmap, compare_stats_for_k, \
                          merge_sankey, sig_cluster_diagnostics, \
                       volcano, check_abundance_bias, check_total_abundance_bias
+from .score_and_merge.cluster_classifier import plot_curves
 
 
